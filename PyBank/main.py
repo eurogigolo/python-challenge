11c954e521e030b8eac53ab85cf5e82a70f1bb6e
eurogigolo <45404907+eurogigolo@users.noreply.github.com>
2020-06-25 15:52:11 -0400

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,69 @@
 import os
 import csv
+import statistics
+
+#task 1.1
+dates = []
+number_months = 0
+
+#task 1.2
+total = 0
+profitlist = []
+
+#task 1.3
+change = []
 
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath,'r')as csvfile:
-    csvreader = csv.DictReader(csvfile, delimiter=',')
-    #header = next(csvreader)
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
     
-    #task 1.1
-    number_months = 0
-    #task 1.2
-    total = 0
-    #task 1.3
-    profitlist = []
-    change = 0
-    changetotal = 0
+
+    
     for row in csvreader:
-        number_months = number_months + 1
-        total = int(row['Profit/Losses']) + total
+        number_months = number_months + 1 #adds 1 for every row in csv
 
-        greatestincrease = max(row['Profit/Losses'])
-        greatestdecrease = min(row['Profit/Losses'])
-        increasedate = []
-        decreasedate = []
+        dates.append(row[0]) #adds all dates to empty list
 
-        #print(row['Profit/Losses'])
-        if row['Profit/Losses'] == greatestincrease:
-            increasedate.append(row['Date'])
-        if row['Profit/Losses'] == greatestdecrease:
-            decreasedate.append(row['Date'])
-        #change = row - (row + 1)) + change
-        #changetotal = change / number_months
+        total = int(row[1]) + total #sums the total profit/loss clumns
+        
+        
+        profitlist.append(int(row[1]))#adds all profits and losses to list
+        maxgreatest = max(profitlist)#max value of the list
+        minleast = min(profitlist)#min value of list
+
+        mean1 = statistics.mean(profitlist)
+        change.append(int(row[1]) - mean1)
+        changetotal = round(statistics.mean(change),2)
 
 
+        if int(row[1]) == maxgreatest:#determines the date of the great profit or loss
+            greatdate = row[0]
+        elif int(row[1]) == minleast:
+            leastdate = row[0]
+
+
+
+
+    print("Financial Analysis")
+    print("--------------------------")
     print(f"Total Months: {number_months}")
     print(f"Total Profit: ${total}")
-    #print(f"Average Change: ${change}")
-    print(f"Greatest Increase in Profits: {increasedate}({greatestincrease})")
-    print(f"Greatest Increase in Profits: {decreasedate} ({greatestdecrease})")
+    print(f"Average Change: ${changetotal}")
+    print(f"Greatest Increase in Profits: {greatdate} (${maxgreatest})")
+    print(f"Greatest Decrease in Profits: {leastdate} (${minleast})")
+
+    file = open('analysis/pythonhw.txt','w')
+
+    file.write("Financial Analysis\n")
+    file.write("--------------------------\n")
+    file.write(f"Total Months: {number_months}\n")
+    file.write(f"Total Profit: ${total}\n")
+    file.write(f"Average Change: ${changetotal}\n")
+    file.write(f"Greatest Increase in Profits: {greatdate} (${maxgreatest})\n")
+    file.write(f"Greatest Decrease in Profits: {leastdate} (${minleast})\n")
 
 
 
